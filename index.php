76776a458732d1205ff49053fa443a0a8e85f8ba
Batman en C103 <20029433+Stacced@users.noreply.github.com>
2019-04-16 10:55:23 +0200

    core: added jquery to index

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-
 <html>
     <head>
         <meta charset="UTF-8">
+        
         <title>World Clock</title>
     </head>
     <body>
         
     </body>
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 </html>
